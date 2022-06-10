e17d45858390825a52a3fa05e3edc2a2edb11c27
Agung Satya <i.g.b.n.satyawibawa@gmail.com>
2022-06-10 13:55:23 +0800

    Challange 7

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let resultAfterWatch = [];
+
+  await promiseTheaterIXX().then((data) => {
+    resultAfterWatch.push(...data);
+  });
+
+  await promiseTheaterVGC().then((data) => {
+    resultAfterWatch.push(...data);
+  });
+  return resultAfterWatch.filter((review) => review.hasil === emosi).length;
+};
 
 module.exports = {
   promiseOutput,
